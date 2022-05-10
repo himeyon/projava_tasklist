2bb083db745d9da57ad3224160e675ca3b70ca01
m_katagiri <m_katagiri@takt.co.jp>
2022-05-10 15:18:49 +0900

    Initial commit

---
+++ src/main/java/jp/co/takt/projava/tasklist/TasklistApplication.java
@@ -0,0 +1,13 @@
+package jp.co.takt.projava.tasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/takt/projava/tasklist/TasklistApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.takt.projava.tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
