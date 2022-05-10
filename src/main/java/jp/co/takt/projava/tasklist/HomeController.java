92163b8c403a3b1cf98aee96faec17f1849bebbc
m_katagiri <m_katagiri@takt.co.jp>
2022-05-10 16:01:37 +0900

    21_5_5 HomeControllerのhelloメソッドを修正する HTMLテンプレートに値をバインドする

--- src/main/java/jp/co/takt/projava/tasklist/HomeController.java
+++ src/main/java/jp/co/takt/projava/tasklist/HomeController.java
@@ -1,24 +1,21 @@
 package jp.co.takt.projava.tasklist;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value="/hello")
-    @ResponseBody
-        String hello() {
-        return """
-            <html>
-                <head><title>Hello</title></head>
-                <body>
-                    <h1>Hello</h1>
-                    It works!<br>
-                    現在時刻は%sです。
-                </body>
-            </html>
-        """.formatted(LocalDateTime.now());
+
+    /**
+     * hello.
+     * @param model モデル
+     * @return htmlテンプレート
+     */
+    @RequestMapping(value = "/hello")
+    String hello(final Model model) {
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
     }
 }
