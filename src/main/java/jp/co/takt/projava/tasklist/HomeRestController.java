2bcc5a0b21fa580667cad6e81281f30af8f4b543
m_katagiri <m_katagiri@takt.co.jp>
2022-05-10 15:33:59 +0900

    21_4_2 タスクを追加するエンドポイントの作成

--- src/main/java/jp/co/takt/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/co/takt/projava/tasklist/HomeRestController.java
@@ -1,11 +1,14 @@
 package jp.co.takt.projava.tasklist;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class HomeRestController {
@@ -20,4 +23,17 @@ public class HomeRestController {
 
     record TaskItem(String id, String task, String deadline, boolean done) {}
     private List<TaskItem> taskItems = new ArrayList<>();
+
+    /**
+     * タスクを追加する.
+     * @param task タスク名
+     * @param deadline タスクの期限
+     * @return タスク追加メッセージ
+     */
+    @GetMapping("/restadd")
+    String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {
+        final String id = UUID.randomUUID().toString().substring(0, 8);
+        taskItems.add(new TaskItem(id, task, deadline, false));
+        return "タスクを追加しました。";
+    }
 }
