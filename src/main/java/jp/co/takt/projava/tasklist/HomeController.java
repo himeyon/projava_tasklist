1ffff533ec42c6515df1941bcf8315e7920a21c5
himeyon <mizuki15@gmail.com>
2022-05-13 13:23:19 +0900

    21_5_7 HomeControllerにタスクの一覧表示機能のエンドポイントを実装する

--- src/main/java/jp/co/takt/projava/tasklist/HomeController.java
+++ src/main/java/jp/co/takt/projava/tasklist/HomeController.java
@@ -2,8 +2,11 @@ package jp.co.takt.projava.tasklist;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -17,5 +20,19 @@ public class HomeController {
     String hello(final Model model) {
         model.addAttribute("time", LocalDateTime.now());
         return "hello";
+    }
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<TaskItem> taskItems = new ArrayList<>();
+
+    /**
+     * タスクの一覧表示.
+     * @param model モデル
+     * @return タスク一覧
+     */
+    @GetMapping("/list")
+    String listItems(final Model model) {
+        model.addAttribute("taskList", taskItems);
+        return "home";
     }
 }
