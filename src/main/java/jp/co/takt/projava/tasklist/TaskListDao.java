0a991157311368af21685445761d0cd2d3b79212
himeyon <mizuki15@gmail.com>
2022-05-17 17:25:31 +0900

    22_3_4 データベース操作用のクラスを作成する

---
+++ src/main/java/jp/co/takt/projava/tasklist/TaskListDao.java
@@ -0,0 +1,52 @@
+package jp.co.takt.projava.tasklist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+import jp.co.takt.projava.tasklist.HomeController.TaskItem;
+
+@Service
+public class TaskListDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    /**
+     * コンストラクタ
+     * @param jdbcTemplate JDBCテンプレート
+     */
+    @Autowired
+    TaskListDao(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    /**
+     * 登録処理.
+     * @param taskItem タスク
+     */
+    public void add(final TaskItem taskItem) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(taskItem);
+        var insert = new SimpleJdbcInsert(jdbcTemplate).withTableName("tasklist");
+        insert.execute(param);
+    }
+
+    /**
+     * 検索処理
+     * @return タスクリスト
+     */
+    public List<TaskItem> findAll() {
+        final String query = "SELECT * FROM tasklist";
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
+        return result.stream()
+            .map((Map<String, Object> row) -> new TaskItem(
+                row.get("id").toString(),
+                row.get("task").toString(),
+                row.get("deadline").toString(),
+                (Boolean) row.get("done"))).toList();
+    }
+}
