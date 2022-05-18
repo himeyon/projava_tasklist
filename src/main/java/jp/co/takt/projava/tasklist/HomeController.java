d1a71eec1e6d94991a6e40f5923d33b1919a050f
himeyon <mizuki15@gmail.com>
2022-05-18 09:13:22 +0900

    22_3_5 HomeControllerクラスを修正する(DAOを利用するように)

--- src/main/java/jp/co/takt/projava/tasklist/HomeController.java
+++ src/main/java/jp/co/takt/projava/tasklist/HomeController.java
@@ -1,5 +1,6 @@
 package jp.co.takt.projava.tasklist;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Controller
 public class HomeController {
+
+    private final TaskListDao taskListDao;
+
+    /**
+     * コンストラクタ
+     * @param dao タスクリストDAO
+     */
+    @Autowired
+    HomeController(final TaskListDao dao) {
+        this.taskListDao = dao;
+    }
 
     /**
      * hello.
@@ -26,7 +36,6 @@ public class HomeController {
     }
 
     record TaskItem(String id, String task, String deadline, boolean done) {}
-    private List<TaskItem> taskItems = new ArrayList<>();
 
     /**
      * タスクの一覧表示.
@@ -35,7 +44,7 @@ public class HomeController {
      */
     @GetMapping("/list")
     String listItems(final Model model) {
-        model.addAttribute("taskList", taskItems);
+        model.addAttribute("taskList", taskListDao.findAll());
         return "home";
     }
 
@@ -48,7 +57,8 @@ public class HomeController {
     @GetMapping("/add")
     String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {
         final String id = UUID.randomUUID().toString().substring(0, 8);
-        taskItems.add(new TaskItem(id, task, deadline, false));
+        final var item = new TaskItem(id, task, deadline, false);
+        taskListDao.add(item);
         return "redirect:/list";
     }
 }
